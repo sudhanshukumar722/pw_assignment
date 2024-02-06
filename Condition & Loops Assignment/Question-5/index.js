15441202e5e5b8335cfbcf24f115552f4dc3072a
sudhanshukumar722 <129382440+sudhanshukumar722@users.noreply.github.com>
2024-02-06 11:15:01 +0530

    Create index.js

---
+++ Condition & Loops Assignment/Question-5/index.js
@@ -0,0 +1,15 @@
+
+//Printing in ascending Order
+let n=1;
+while(n<=25){
+    console.log(n);
+    n++;
+}
+
+
+//Printing Number in decending Order
+let n2=25;
+while(n2>0){
+    console.log(n2);
+    n2--;
+}
