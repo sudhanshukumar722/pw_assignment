4169247cc54e1b967309e94cedf7e41217896f76
sudhanshukumar722 <129382440+sudhanshukumar722@users.noreply.github.com>
2024-02-06 10:48:19 +0530

    Create index.js

---
+++ Condition & Loops Assignment/Question-2/index.js
@@ -0,0 +1,12 @@
+let marks=50;
+
+if(marks>=90){
+    console.log("A Grade");
+}else if(marks>=70&&marks<90){
+    console.log("B Grade");
+}else if(marks>=50&&marks<70){
+    console.log("C Grade")
+}
+else{
+    console.log("F Grade");
+}
